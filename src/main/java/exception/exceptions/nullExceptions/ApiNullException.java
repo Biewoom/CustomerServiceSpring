5d29691ebea95cc7c2f62df2dfc6d4e4418f0635
JeongminYu <likemin0142@gmail.com>
2020-08-09 23:46:55 +0900

    start!

---
+++ src/main/java/exception/exceptions/nullExceptions/ApiNullException.java
@@ -0,0 +1,17 @@
+package exception.exceptions.nullExceptions;
+
+import exception.ApiException;
+
+public class ApiNullException extends ApiException {
+    public ApiNullException(String request) {
+        super(request);
+    }
+
+    public ApiNullException(String request, String layerName) {
+        super(request, layerName);
+    }
+
+    public ApiNullException(String request, String layerName, String serviceName) {
+        super(request, layerName, serviceName);
+    }
+}
